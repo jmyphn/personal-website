9f3990e981ee0a891b3087d396f06d433aee3896
jp2369 <jp2369@cornell.edu>
2025-05-24 02:41:16 -0400

    favicon idk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "jimmy phan",
   description: "personal website",
+  icons: {
+    icon: "./favicon.ico",
+  }
 };
 
 export default function RootLayout({
